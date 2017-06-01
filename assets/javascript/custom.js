70ce73397a1c8f9e5beb418a114608ed4d4685d0
Lindsay Larkin Larkin <lindsay.larkin@gmail.com>
2017-05-31 20:50:56 -0600

    collapsibles and comments

--- assets/javascript/custom.js
+++ assets/javascript/custom.js
@@ -5,8 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
     aside = document.getElementsByClassName('aside')[0]
     sectionBody = document.getElementsByClassName('section-body')[0]
 
+    // this needs to account for aside width, which is no longer fixed at 250px?
     sectionBody.style.width = (width - 250) + 'px'
-    aside.style.minHeight = sectionBody.offsetHeight + 25 + 'px'
+    aside.style.minHeight = sectionBody.offsetHeight + 'px'
+    // how do I make minHeight 100%?
   }
 
   window.setTimeout(setAsideHeight, 500)
