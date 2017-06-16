b00d8982b85bd7bba839116c655f570ce5d459ee
Lindsay Larkin Larkin <lindsay.larkin@gmail.com>
2017-06-15 21:23:11 -0600

    plays with css and adds js function for section body

--- assets/javascript/custom.js
+++ assets/javascript/custom.js
@@ -11,4 +11,17 @@ $(document).ready(function() {
   }
 
   setAsideHeight()
+
+    function setSectionBodyHeight() {
+    navbarOffset = 80;
+    nameHeaderOffset = 75;
+    footerOffset = 60;
+    totalOffset = navbarOffset + nameHeaderOffset + footerOffset;
+
+    sectionBody = document.getElementsByClassName('section-body')[0]
+
+    sectionBody.style.minHeight = window.outerHeight - totalOffset + 'px'
+  }
+
+  setSectionBodyHeight()
 });
